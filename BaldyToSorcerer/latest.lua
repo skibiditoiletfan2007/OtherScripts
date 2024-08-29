d23b2e3bfe4f45001605a4e72471543de0d93fe4
skibiditoiletfan2007 <148602068+skibiditoiletfan2007@users.noreply.github.com>
2024-08-28 22:41:35 -0700

    Update latest.lua

--- BaldyToSorcerer/latest.lua
+++ BaldyToSorcerer/latest.lua
@@ -1,1 +1,7 @@
-game.Players.LocalPlayer:Kick("Script is currently migrating Join .gg/pznM2AfdJz")
+if not _G.settings then
+  _G.settings = {
+      ["RedStartupId"] = "rbxassetid://1177475221",
+      ["RedHitId"] = "rbxassetid://8625377966",
+  }
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/skibiditoiletfan2007/BaldyToSorcerer/main/Latest.lua"))()
